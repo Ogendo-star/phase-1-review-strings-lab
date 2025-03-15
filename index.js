eb106ccb8032c1c0b15e409d887d02e1a742257e
Masela Ogendo <puroselah@gmail.com>
2025-03-15 15:49:53 +0300

    feat:reviewed a string and used the slice operator

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Jenny Serah';
+const welcomeMessage = "Welcome to Flatbook, + Jenny Serah!"
+
+const excitedWelcomeMessage =welcomeMessage.toUpperCase();
+
+currentUser.slice(0, 1);
+const shortGreeting= `Welcome, ${currentUser.slice(0,1)}!`;
